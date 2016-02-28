272fb479a567c27de6f3f4b4e7b794f79daa322a
puterakahfi <puterakahfi@gmail.com>
2016-02-28 11:04:26 +0700

    added nebula background

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,19 +1,24 @@
 'use strict';
 
-function Custom(){
-	self = this;
-	self.init= function(){
-		self.ajaxPreloader();
-	};
+function Custom() {
+    self = this;
+    self.init = function () {
+        self.ajaxPreloader();
+        self.tooltips();
+    };
 
-	self.ajaxPreloader=function(){
-	$(window).load(function() {
-        // will first fade out the loading animation
-	$("#preloader").fadeOut();
-        // will fade out the whole DIV that covers the website.
-	$("#preloader").delay(1000).fadeOut("slow");
-})
-	}
+    self.ajaxPreloader = function () {
+        $(window).load(function () {
+            // will first fade out the loading animation
+            $("#preloader").fadeOut();
+            // will fade out the whole DIV that covers the website.
+            $("#preloader").delay(1000).fadeOut("slow");
+        })
+    };
+
+    self.tooltips = function () {
+        $('.btn').tooltip();
+    };
 
 
 
