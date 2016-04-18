4614853d292f502a8dfd67a9571568ced9c51aab
puterakahfi <puterakahfi@gmail.com>
2016-04-18 21:29:14 +0700

    modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+requireDir = require('require-dir')
+concat = require('gulp-concat')
+sass = require('gulp-sass');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-clean-css');
+var inject = require('gulp-inject');
+var changed = require('gulp-changed');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var requireDir = require('require-dir');
+
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
+
+
+gulp.task('copyfonts', function() {
+	   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
+      .pipe(gulp.dest('./dist/fonts'));
 });
+
+
