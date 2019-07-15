038f1c28108a46fd8142bb04644281216c64cef1
putera kahfi <puterakahfi@gmail.com>
2019-07-15 14:16:08 +0700

    add build with

--- src/components/molecules/SmallFooter.vue
+++ src/components/molecules/SmallFooter.vue
@@ -3,28 +3,27 @@
     <div class="is-size-7">
       <div class="buttons are-small">
         <a
-          class="button is-light has-bg-transparent"
+          class="button is-light"
           target="_blank"
           href="https://github.com/puterakahfi/puterakahfi.github.io/issues"
         >
-          <i class="fas fa-bug"></i> found issue ?
+          <i class="fas fa-bug has-margin-right-5"></i> found issue ?
         </a>
-        <a
-          target="_blank"
-          class="button is-light has-bg-transparent"
-          href="https://github.com/puterakahfi"
-        >
+        <a target="_blank" class="button is-light" href="https://github.com/puterakahfi">
           <i class="fab fa-github"></i>
         </a>
-        <a
-          target="_blank"
-          class="button is-light has-bg-transparent"
-          href="https://facebook.com/codejunior"
-        >
+        <a target="_blank" class="button is-light" href="https://facebook.com/codejunior">
           <i class="fab fa-facebook"></i>
         </a>
       </div>
     </div>
+
+    <p class="subtitle is-7 has-margin-top-10 has-text-grey">
+      made with
+      <i class="fas fa-heart has-text-danger"></i> with
+      <a href="https://gridsome.org" target="_blank">gridsome</a> and
+      <a href="https://bulma.io/" target="_blank">bulma</a>
+    </p>
   </section>
 </template>
 
