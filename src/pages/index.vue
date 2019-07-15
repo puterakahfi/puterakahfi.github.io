90b2e25bc85e2fe9c0723663dc70244ec9cbb6c8
puterakahfi <puterakahfi@gmail.com>
2019-07-16 00:48:43 +0700

    change layout into 10x2 columns'

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,21 +2,21 @@
   <Layout :show-logo="false" :show-header="false" class="container">
     <!-- Author intro -->
     <div class="columns">
-      <div class="column is-9">
+      <div class="column is-10">
         <LatestPost data-aos="fade-down" :post="latestPost" />
       </div>
 
-      <div class="colum is-3 has-padding-5">
+      <div class="colum is-2 has-padding-5">
         <div data-aos="fade-up">
           <g-image
             class="has-margin-10"
             src="https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/buffer_wq43.svg"
             alt
           ></g-image>
-          <h1 class="title is-4 has-text-left has-margin-bottom-5">Free Learning Resources</h1>
+          <h1 class="title is-5 has-text-left has-margin-bottom-5">Free Learning Resources</h1>
           <p
             class="has-margin-top-5 is-size-7"
-          >Koleski berbagai link, tutorial, free e-books terkait pemrograman, software development</p>
+          >Koleksi berbagai link, tutorial, free e-books terkait pemrograman, software development</p>
           <a
             href="https://puterakahfi.github.io/learning-source/"
             target="_blank"
@@ -30,7 +30,7 @@
             alt
           ></g-image>
           <h1
-            class="title is-4 has-text-left has-margin-bottom-5"
+            class="title is-5 has-text-left has-margin-bottom-5"
           >Awesome Javascript Developer Indonesia</h1>
           <p
             class="has-margin-top-5 is-size-7"
